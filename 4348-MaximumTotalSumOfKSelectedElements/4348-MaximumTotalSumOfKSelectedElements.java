19c3a41de216ab89dad7f2c48a8e98f25add7954
Sujithasri1318 <24243109@drngpit.ac.in>
2026-07-09 10:06:17 +0530

    Create 4348-MaximumTotalSumOfKSelectedElements

---
+++ 4348-MaximumTotalSumOfKSelectedElements/4348-MaximumTotalSumOfKSelectedElements.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 10:06:17 AM
+class Solution {
+    public long maxSum(int[] nums, int k, int mul) {
+        Arrays.sort(nums);
+
+        long ans = 0;
+        int currentMul = mul;
+
+        for(int i = nums.length - 1; i>= nums.length - k; i--){
+            long add = nums[i];
+            long multiply = 1L * nums[i] * currentMul;
+
+            ans += Math.max(add, multiply);
+            currentMul--;
+        }
+
+        return ans ;
+    }
+}
