8c6a433455025f9bd39dc3fbdba5f89617793a66
Sujithasri1318 <24243109@drngpit.ac.in>
2026-07-09 10:06:36 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:06:36 AM
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
